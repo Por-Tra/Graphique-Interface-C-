8ca82c5a5677a1ae1b9791048ca47f57d60fa1dd
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2025-08-01 00:28:43 +0200

    Ajout de la première fenetre ouvrable et ajustable

--- GUI/GUI/GUI.cpp
+++ GUI/GUI/GUI.cpp
@@ -1,20 +1,65 @@
-// GUI.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+#include <windows.h>  
 
-#include <iostream>
+// Déclaration de la fonction WindowProc  
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)  
+{  
+  switch (uMsg)  
+  {  
+      case WM_DESTROY:  
+          PostQuitMessage(0);  
+          return 0;  
 
-int main()
-{
-    std::cout << "Hello World!\n";
+      default:  
+          return DefWindowProc(hwnd, uMsg, wParam, lParam);  
+  }  
+}  
+
+int main()  
+{  
+  // Enregistrement de la classe de fenêtre.  
+  const wchar_t CLASS_NAME[] = L"Sample Window Class";  
+
+  WNDCLASS wc = {};  
+
+  wc.lpfnWndProc = WindowProc;  
+  wc.hInstance = GetModuleHandle(NULL);  
+  wc.lpszClassName = CLASS_NAME;  
+
+  if (!RegisterClass(&wc))  
+  {  
+      return -1;  
+  }
+
+  HINSTANCE hInstance = GetModuleHandle(NULL); // Définition de hInstance
+
+  HWND hwnd = CreateWindowEx(
+      0,                              // Optional window styles.
+      CLASS_NAME,                     // Window class
+      L"Learn to Program Windows",    // Window text
+      WS_OVERLAPPEDWINDOW,            // Window style
+
+      // Size and position
+      CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+      NULL,       // Parent window    
+      NULL,       // Menu
+      hInstance,  // Instance handle
+      NULL        // Additional application data
+  );
+
+  if (hwnd == NULL)
+  {
+      return 0;
+  }
+
+  ShowWindow(hwnd, SW_SHOW);
+
+  MSG msg = {};
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+  }
+
+  return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
