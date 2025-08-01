475c05fcf41dc5c6deb9b341bb9143eb5433ed9e
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2025-08-01 15:01:00 +0200

    qlq modif encore

--- GUI/GUI/GUI.cpp
+++ GUI/GUI/GUI.cpp
@@ -14,28 +14,44 @@
 // Déclaration de la variable globale pour le menu
 HMENU hMenu;
 TCHAR szFileName[MAX_PATH] = { 0 }; // Buffer pour le chemin du fichier (MAX_PATH = 260)
+std::wstring textBuffer; // Buffer pour stocker le texte
+const int MAX_TEXT_LENGTH = 10000; // Limite du texte
+HBITMAP hBitmap = nullptr; // Handle pour l'image
 
 
 // Déclaration des fonctions
 void AddMenu(HWND hwnd);
+void AddControls(HWND hwnd);
+
 
 
 
 // Fonction de gestion des événements
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
+    static HWND hEdit = nullptr; // Handle du contrôle EDIT
 
-	case WM_CREATE:
+    switch (uMsg) {
+    case WM_CREATE:
         AddMenu(hwnd);
-		return 0;
+        AddControls(hwnd); // Ajoute les contrôles
+        hEdit = GetDlgItem(hwnd, 100); // Récupère le handle du contrôle EDIT (ID 100)
+        if (hEdit) {
+            SendMessageW(hEdit, WM_SETTEXT, 0, (LPARAM)textBuffer.c_str()); // Initialise avec textBuffer
+        }
+        return 0;
 
     case WM_COMMAND:
     {
-        switch (wParam) 
+        switch (wParam)
         {
         case FILE_MENU_NEW:
-			MessageBeep(MB_OK);
-			break;
+            if (hEdit) {
+                SendMessageW(hEdit, WM_SETTEXT, 0, (LPARAM)L""); // Efface le texte
+                textBuffer.clear();
+            }
+            MessageBeep(MB_OK);
+            break;
+
         case FILE_MENU_OPEN:
         {
             OPENFILENAME ofn = { 0 };
@@ -43,32 +59,50 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
             ofn.lStructSize = sizeof(OPENFILENAME);
             ofn.hwndOwner = hwnd;
-            ofn.lpstrFilter = L"Tous les fichiers (*.*)\0*.*\0Textes (*.txt)\0*.txt\0\0"; // Filtre des types de fichiers
+            ofn.lpstrFilter = L"Tous les fichiers (*.*)\0*.*\0Textes (*.txt)\0*.txt\0Images (*.bmp)\0*.bmp\0\0";
             ofn.lpstrFile = szFileName;
             ofn.nMaxFile = MAX_PATH;
-            ofn.Flags = OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY; // Options : fichier doit exister, chemin valide
-            ofn.lpstrDefExt = L"txt"; // Extension par défaut
+            ofn.Flags = OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY;
 
             if (GetOpenFileName(&ofn)) {
-                // Si l'utilisateur a sélectionné un fichier, affiche le chemin
-                MessageBoxW(hwnd, szFileName, L"Fichier sélectionné", MB_OK | MB_ICONINFORMATION);
-                // Ici, tu peux ajouter la logique pour ouvrir le fichier (par exemple, avec un ifstream)
+                std::wstring filePath(szFileName);
+                if (filePath.length() >= 4 && filePath.substr(filePath.length() - 4) == L".txt") {
+                    std::wifstream file(szFileName, std::ios::in);
+                    if (file.is_open()) {
+                        textBuffer.assign((std::istreambuf_iterator<wchar_t>(file)), std::istreambuf_iterator<wchar_t>());
+                        file.close();
+                        if (hEdit) {
+                            SendMessageW(hEdit, WM_SETTEXT, 0, (LPARAM)textBuffer.c_str()); // Mets à jour le contrôle
+                        }
+                        InvalidateRect(hwnd, NULL, TRUE);
+                    }
+                    else {
+                        MessageBoxW(hwnd, L"Erreur lors de l'ouverture du fichier texte", L"Erreur", MB_OK | MB_ICONERROR);
+                    }
+                }
+                else if (filePath.length() >= 4 && filePath.substr(filePath.length() - 4) == L".bmp") {
+                    if (hBitmap) DeleteObject(hBitmap);
+                    hBitmap = (HBITMAP)LoadImageW(NULL, szFileName, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_DEFAULTSIZE);
+                    if (hBitmap) {
+                        InvalidateRect(hwnd, NULL, TRUE);
+                        MessageBoxW(hwnd, L"Image chargée avec succès !", L"Info", MB_OK | MB_ICONINFORMATION);
+                    }
+                    else {
+                        MessageBoxW(hwnd, L"Erreur lors du chargement de l'image", L"Erreur", MB_OK | MB_ICONERROR);
+                    }
+                }
             }
             else {
-                // Vérifie si l'utilisateur a annulé ou s'il y a une erreur
-                if (CommDlgExtendedError() == 0) 
-                {
-					MessageBeep(MB_ICONINFORMATION);
+                if (CommDlgExtendedError() == 0) {
                     MessageBoxW(hwnd, L"Annulé par l'utilisateur", L"Info", MB_OK);
                 }
-                else 
-                {
-					MessageBeep(MB_ICONERROR);
+                else {
                     MessageBoxW(hwnd, L"Erreur lors de l'ouverture du dialogue", L"Erreur", MB_OK | MB_ICONERROR);
                 }
             }
             break;
         }
+
         case FILE_MENU_SAVE:
         {
             OPENFILENAME ofn = { 0 };
@@ -76,18 +110,25 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
             ofn.lStructSize = sizeof(OPENFILENAME);
             ofn.hwndOwner = hwnd;
-            ofn.lpstrFilter = L"Tous les fichiers (*.*)\0*.*\0Textes (*.txt)\0*.txt\0\0"; // Filtre des types de fichiers
+            ofn.lpstrFilter = L"Tous les fichiers (*.*)\0*.*\0Textes (*.txt)\0*.txt\0\0";
             ofn.lpstrFile = szFileName;
             ofn.nMaxFile = MAX_PATH;
-            ofn.Flags = OFN_OVERWRITEPROMPT | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY; // Demande confirmation si fichier existe
-            ofn.lpstrDefExt = L"txt"; // Extension par défaut
+            ofn.Flags = OFN_OVERWRITEPROMPT | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY;
+            ofn.lpstrDefExt = L"txt";
 
             if (GetSaveFileName(&ofn)) {
-                // Ouvre le fichier en mode écriture
+                if (hEdit) {
+                    int len = SendMessageW(hEdit, WM_GETTEXTLENGTH, 0, 0);
+                    if (len > 0) {
+                        wchar_t* buffer = new wchar_t[len + 1];
+                        SendMessageW(hEdit, WM_GETTEXT, len + 1, (LPARAM)buffer);
+                        textBuffer = buffer;
+                        delete[] buffer;
+                    }
+                }
                 std::wofstream file(szFileName, std::ios::out);
                 if (file.is_open()) {
-                    // Écrit un exemple de texte (tu peux modifier ça selon ton besoin)
-                    file << L"Contenu sauvegardé à " << L"02:04 PM CEST, Vendredi 01 Août 2025" << std::endl;
+                    file << textBuffer;
                     file.close();
                     MessageBoxW(hwnd, L"Fichier sauvegardé avec succès !", L"Sauvegarde", MB_OK | MB_ICONINFORMATION);
                 }
@@ -105,49 +146,50 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             }
             break;
         }
-		case FILE_MENU_EXIT:
-			if (MessageBoxW(hwnd, L"Voulez-vous vraiment quitter l'application ?", L"Quitter", MB_OKCANCEL | MB_ICONQUESTION) == IDOK) {
-				DestroyWindow(hwnd);
-			}
-			break;
-        }
 
+        case FILE_MENU_EXIT:
+            if (MessageBoxW(hwnd, L"Voulez-vous vraiment quitter l'application ?", L"Quitter", MB_OKCANCEL | MB_ICONQUESTION) == IDOK) {
+                DestroyWindow(hwnd);
+            }
+            break;
+        }
         return 0;
     }
 
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-
-    /*case WM_CLOSE:
-        if (MessageBoxW(hwnd, L"QUITTER L'APPLICATION ?", L"APP", MB_OKCANCEL) == IDOK) {
-            DestroyWindow(hwnd);
-        }
-        return 0;*/
-
-    case WM_LBUTTONDOWN:
-        std::wcout << L"CLIC GAUCHE\n";
-        return 0;
-
-    case WM_RBUTTONDOWN:
-        std::wcout << L"CLIC DROIT\n";
-        return 0;
-
-    case WM_SIZE: {
+    case WM_SIZE:
+    {
         int width = LOWORD(lParam);
         int height = HIWORD(lParam);
-        // À implémenter : ajuster les widgets
+        if (hEdit) {
+            // Redimensionne le contrôle EDIT pour s'adapter à la fenêtre
+            MoveWindow(hEdit, 10, 10, width - 20, height - 60, TRUE); // Laisse une marge
+        }
         std::wcout << L"Redimensionnement : " << width << L"x" << height << L"\n";
         return 0;
     }
 
-    case WM_PAINT: {
+    case WM_PAINT:
+    {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
+        // Affiche l'image si chargée (en dessous du contrôle EDIT)  
+        if (hBitmap) {
+            HDC memDC = CreateCompatibleDC(hdc);
+            SelectObject(memDC, hBitmap);
+            BITMAP bm;
+            GetObject(hBitmap, sizeof(bm), &bm);
+            BitBlt(hdc, 10, 590, bm.bmWidth, bm.bmHeight, memDC, 0, 0, SRCCOPY); // Ajuste la position  
+            DeleteDC(memDC);
+        }
+        EndPaint(hwnd, &ps); // Correction : passez hwnd comme premier argument et &ps comme second  
         return 0;
     }
+
+    case WM_DESTROY:
+        if (hBitmap) DeleteObject(hBitmap);
+        PostQuitMessage(0);
+        return 0;
 
     default:
         return DefWindowProcW(hwnd, uMsg, wParam, lParam);
@@ -185,7 +227,30 @@ void AddMenu(HWND hwnd)
    SetMenu(hwnd, hMenu);  
 }
 
+// Cette fonction peut être utilisée pour ajouter des contrôles supplémentaires à la fenêtre
+// Par exemple, des boutons, des champs de texte, etc.
+//void AddControls(HWND hwnd) 
+//{
+//	//CreateWindowW(L"STATIC", L"Entre le texte ici", WS_VISIBLE | WS_CHILD | SS_CENTER | WS_BORDER,
+//	//	50, 50, 200, 20, hwnd, nullptr, nullptr, nullptr); // Exemple de texte statique
+//	CreateWindowW(L"EDIT", L"", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_LEFT, 50, 80, 200, 20, hwnd, nullptr, nullptr, nullptr); // Exemple de champ de texte
+//
+//	/*CreateWindowW(L"BUTTON", L"Button 1", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+//		10, 10, 100, 30, hwnd, (HMENU)1, nullptr, nullptr);*/ // Exemple de bouton
+//}
+void AddControls(HWND hwnd)
+{
+    // Crée un champ de texte multiligne avec défilement
+    HWND hEdit = CreateWindowW(L"EDIT", L"",
+        WS_VISIBLE | WS_CHILD | WS_VSCROLL | WS_HSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL | WS_BORDER,
+        10, 10, 780, 580, // Ajuste les dimensions pour presque toute la fenêtre (laisse de la marge)
+        hwnd, (HMENU)100, nullptr, nullptr); // ID 100 pour le contrôle
 
+    if (hEdit) {
+        // Définit la police par défaut (facultatif, améliore la lisibilité)
+        SendMessageW(hEdit, WM_SETFONT, (WPARAM)GetStockObject(DEFAULT_GUI_FONT), TRUE);
+    }
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
     // Enregistrement de la classe de fenêtre
